b46b162c919f6840e076ab55ba1977d59756934e
nikitauralev <99537574+nikitauralev@users.noreply.github.com>
2022-06-11 20:49:53 +0300

    Update 02_hw.py

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -1,1 +1,9 @@
-
+n = int(input())
+if n in range(2, 4):
+    print('На лугу пасется', n , "коровы")
+else:
+    temp = n % 10
+    if temp in list(range(5, 10)) + [0]:
+        print('На лугу пасется', n , "коров")
+    if temp == 1:
+        print('На лугу пасется', n , "коровa")
