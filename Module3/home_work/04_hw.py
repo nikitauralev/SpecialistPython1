f4eb67e30bded46623b1ff6b9a25e9096deb7c1e
nikitauralev <99537574+nikitauralev@users.noreply.github.com>
2022-06-17 16:45:10 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,26 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+import math
+numbers = [9, 6, 100, 4, 3]
+lists = []
+# n = int(input('Введите количество чисел: '))
+i = 0
+total = 0
+# while i < n:
+#     k = random.randint(-100, 100)
+#     numbers.insert(i, k)
+#     i += 1
+for number in numbers:
+    if (number > 0):
+        x = math.sqrt(number)
+        total += x
+        #print(total)
+        if (x == int(x)):
+            lists.append(int(x))
+    else:
+        print('')
+print('Начальный список: ', numbers)
+print('Конечный список: ', lists)
