d9bce4c9c9439b882fdf02ae185f04ffb61d2094
nikitauralev <99537574+nikitauralev@users.noreply.github.com>
2022-06-19 16:55:37 +0300

    Update 03_task_func.py

--- Module4/practice/funcs_part1/03_task_func.py
+++ Module4/practice/funcs_part1/03_task_func.py
@@ -1,10 +1,10 @@
 # Даны координаты двух точек на плоскости.
 # Напишите функцию нахождения расстояния между двумя точками.
 
+import math
 def distance(x1, y1, x2, y2):
-    # TODO: your code here
-    pass
-
+    len_xy = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    return len_xy
 
 # Тестируем функцию
 print(distance(2, 4, 2, 9))
